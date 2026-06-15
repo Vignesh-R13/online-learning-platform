30a9163ac9de22cd44359f3b9b076db92985c6fd
Vignesh-R13 <vigneshravilakshmi@gmail.com>
2026-06-15 16:07:35 +0530

    Fix user model import case

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
-const User = require("../models/User");
+const User = require("../models/user");
 
 // REGISTER USER
 exports.registerUser = async (req, res) => {
